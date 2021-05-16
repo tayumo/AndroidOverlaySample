50da70d5fa1d92159735b88fd51aba97dc2af019
tayumo <tayumo.dev@gmail.com>
2021-05-17 00:45:41 +0900

    add start filtering button

--- app/src/main/java/com/example/overlaysample/MainActivity.java
+++ app/src/main/java/com/example/overlaysample/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.overlaysample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity";
+
+    private Button mStartFilteringButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartFilteringButton = findViewById(R.id.start_filtering_button);
+        mStartFilteringButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "StartFilteringButton is clicked");
+            }
+        });
     }
 
 }
